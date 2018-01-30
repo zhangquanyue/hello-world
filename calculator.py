4fa35e9a8023f8a8f467a73f223c51c3679a5ca7
zhangquanyue <zhang302_8@hotmail.com>
2018-01-30 09:24:50 +0800

    wages

---
+++ calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+Wages = 0
+Wages = sys.argv[1]
+
+if __name__ == '__main__':
+    if Wages < 1500:
+        print('caculating…')
+    elif (1500  <= Wages < 4500):
+        print("caculating…")
+"""
+    elif Wages >= 4500 and < 9000:
+        print('caculating…')
+    elif Wages >= 9000 and < 35000:
+        print('caculating')
+    elif Wages >= 35000 and < 55000:
+        pringt('caculating')
+    elif Wages >= 55000 and < 80000:
+        tax_rate = 0.35
+        taxable_income = Wages - 3500
+        tax_amount_payable = taxable_income * tax_rate -5505
+        print(tax_amount_payable)
+    else Wages > 80000
+        tax_rate = 0.45
+        taxable_income = Wages - 3500
+        tax_amount_payable = taxable_income * tax_rate - 13505 
+"""  
