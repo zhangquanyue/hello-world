695fcccbe9589e15c2c10ef2fd50653b4d9cbf14
zhangquanyue <zhang302_8@hotmail.com>
2018-02-01 00:04:11 +0800

    20180201

--- calculator.py
+++ calculator.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 import sys
-Wages = 1
-Wages = sys.argv[1]
+Wages = int(sys.argv[1])
 
 if __name__ == '__main__':
-    if Wages < 1500:
-        print('caculating…')
-    elif (1500  <= Wages < 4500):
+    taxable_income = Wages - 3500
+    if 0 < taxable_incom < 1500:
+        tax_rate = 0.03
+        tax_amount_payable = taxalbe_income * tax_rate - 0
+        print(format(tax_amount_payable,".2f"))
+    elif (1500  <= taxable_income < 4500):
         print("caculating…")
 """
     elif Wages >= 4500 and < 9000:
@@ -20,8 +22,10 @@ if __name__ == '__main__':
         taxable_income = Wages - 3500
         tax_amount_payable = taxable_income * tax_rate -5505
         print(tax_amount_payable)
-    else Wages > 80000
+    elif Wages > 80000:
         tax_rate = 0.45
         taxable_income = Wages - 3500
         tax_amount_payable = taxable_income * tax_rate - 13505 
 """  
+    else:
+        print("低于起征点，努力赚钱哦")
