20e5deb718c620ae560986c49b912021bac3e98e
zhangquanyue <zhang302_8@hotmail.com>
2018-02-05 00:18:06 +0800

    20180205

--- calculator.py
+++ calculator.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python3
 import sys
-Wages = int(sys.argv[1])
-
-if __name__ == '__main__':
+def Income(income):
+    Wages = income
     taxable_income = Wages - 3500
     if 0 < taxable_income < 1500:
         tax_rate = 0.03
         tax_amount_payable = taxable_income * tax_rate - 0
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif 1500 <= taxable_income < 4500:
         tax_rate = 0.10
         tax_amount_payable = taxable_income * tax_rate - 105
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif 4500 <= taxable_income < 9000:
         tax_rate = 0.20
         tax_amount_payable = taxable_income * tax_rate - 555
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif 9000 <= taxable_income < 35000:
         tax_rate = 0.25
         tax_amount_payable = taxable_income * tax_rate - 1005
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif 35000 <= taxable_income < 55000:
         tax_rate = 0.30
         tax_amount_payable = taxable_income * tax_rate - 2755
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif 55000 <= taxable_income < 80000:
         tax_rate = 0.35
         tax_amount_payable = taxable_income * tax_rate -5505
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     elif taxable_income >= 80000:
         tax_rate = 0.45
         tax_amount_payable = taxable_income * tax_rate -13505
-        print(format(tax_amount_payable,".2f"))
+        return tax_amount_payable
     else:
-        print("低于起征点，努力加油哦")
+        return income
+if __name__ == '__main__':
+    for arg in sys.argv[1:]:
+        employee_id, wage = arg.split(':')
+        try:
+            income = int(wage)
+        except ValueError:
+            print('Parameter Error')
+        after_tax_wages = Income(income)
+        print('{}:{}'.format(employee_id, after_tax_wages)
